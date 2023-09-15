5f547eeafbeaf0f4b71d2ee065fc227603f31e65
TimothyKim1011 <138592141+TimothyKim1011@users.noreply.github.com>
2023-09-16 02:13:20 +0900

    0916

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,7 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
+const toDoFormform = document.querySelector("#todo-form");
 
 const HIDDEN_CLASSNAME = "hidden"; //굳이 바꿔주는 이유가 있을까?
 const USERNAME_KEY = "username";
@@ -12,20 +13,31 @@ const hour = now.getHours();
 function loginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  toDoFormform.classList.remove(HIDDEN_CLASSNAME);
   const loginInputValue = loginInput.value;
   localStorage.setItem(USERNAME_KEY, loginInputValue);
   paintGreetings(loginInputValue);
 }
 
+const hello = [
+  "good to see you!",
+  "Happy to see you!",
+  "Doing well?",
+  "How's your work?",
+  "Do it better",
+];
+
 function paintGreetings(username) {
   if (hour >= 0 && hour < 6) {
-    greeting.innerText = `Good night, ${username}`;
+    greeting.innerText = `It's time to sleep, ${username}`;
   } else if (hour >= 6 && hour < 12) {
     greeting.innerText = `Good morning, ${username}`;
   } else if (hour >= 12 && hour < 18) {
-    greeting.innerText = `Good afternoon, ${username}`;
-  } else if (hour >= 18 && hour < 0) {
-    greeting.innerText = `Good evening, ${username} `;
+    greeting.innerText = `${
+      hello[Math.floor(Math.random() * hello.length)]
+    }, ${username}`;
+  } else if (hour >= 18 && hour < 24) {
+    greeting.innerText = `Keep it work!, ${username}`;
   }
 
   greeting.classList.remove(HIDDEN_CLASSNAME);
@@ -35,8 +47,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   //show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  toDoFormform.classList.add(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", loginSubmit);
 } else {
   // show the greetings
+
   paintGreetings(savedUsername);
 }
